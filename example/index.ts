19eeb5691cee44ebbbb586669667705c883f0893
CaoMeiYouRen <996881204@qq.com>
2025-01-19 23:53:27 +0800

    feat: 更新示例代码以使用动态路径解析，确保图像合成正确引用文件

--- example/index.ts
+++ example/index.ts
@@ -1,8 +1,10 @@
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { synthesizeImage } from '../src/index'
-
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const image = await synthesizeImage({
-    backgroundImagePath: path.resolve('./images/background.png'),
+    backgroundImagePath: path.resolve(__dirname, './images/background.png'),
     text: 'Hello World',
-    outputPath: path.resolve('./output/output.png'),
+    outputPath: path.resolve(__dirname, './output/output.png'),
 })
